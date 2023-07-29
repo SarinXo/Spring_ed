71039c0270a13e89340b558eebb46633cc7ce120
rollo <rollovitch.roll@yandex.ru>
2023-07-29 14:09:22 +0300

    @Autowired and @Value in CompanyRepository. part 1.

--- app/src/main/java/spring_ed/database/repository/CompanyRepository.java
+++ app/src/main/java/spring_ed/database/repository/CompanyRepository.java
@@ -1,6 +1,9 @@
 package spring_ed.database.repository;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import spring_ed.bean_post_processor.Auditing;
 import spring_ed.bean_post_processor.InjectBean;
 import spring_ed.bean_post_processor.Transaction;
@@ -8,14 +11,35 @@ import spring_ed.database.entity.Company;
 import spring_ed.database.pool.ConnectionPool;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 import java.util.Optional;
 
 @Transaction
 @Auditing
 public class CompanyRepository implements CrudRepository<Integer, Company> {
 
-    @InjectBean
-    private ConnectionPool connectionPool;
+
+
+    //имя ConnectionPool - влияет. С самого начала @Autowired захочет инициализировать
+    // с таким же названием, как и название переменной
+    // Так же можно использовать аннотацию @Qualifier("bean_name_or_alias")
+    // с конструктором такая же тема - переданный аргумент должен называться так же как и бин
+    // который инжектим
+    private ConnectionPool pool;
+
+    //спокойно инжектит в коллекцию все существующие бины данного класса
+    //порядком следования можно управлять через аннотацию @Ordered
+    @Autowired
+    private List<ConnectionPool> pools;
+
+    // для inject'а поля или свойства в виде зависимости, то можем использовать
+    // @Value - внутри нее используем EL или SpEL
+    @Value("${db.pool.size}")
+    private int poolSize;
+    @Autowired
+    public CompanyRepository(ConnectionPool p1) {
+        this.pool = p1;
+    }
 
     @PostConstruct
     private void init() {
