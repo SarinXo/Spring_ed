128efb060f5db4943930dae159fc668a4183c5c0
rollo <rollovitch.roll@yandex.ru>
2023-08-01 17:37:07 +0300

    @Import + @ResourceImport

--- app/src/main/java/spring_ed/config/ApplicationConfiguration.java
+++ app/src/main/java/spring_ed/config/ApplicationConfiguration.java
@@ -4,15 +4,23 @@ import org.springframework.context.annotation.ComponentScan;
 
 import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.ImportResource;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 import spring_ed.database.repository.CrudRepository;
+import web.config.WebConfig;
 
 import static org.springframework.context.annotation.FilterType.ANNOTATION;
 import static org.springframework.context.annotation.FilterType.ASSIGNABLE_TYPE;
 import static org.springframework.context.annotation.FilterType.REGEX;
 
+//комбинация всех 3-х вариантов определения bean definitions
+//можем подключать их через эту аннотацию
+@ImportResource("classpath:application.xml")
+//другие Configuration classes
+@Import(WebConfig.class)
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = "spring_ed",
